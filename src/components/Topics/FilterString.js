4e51c22f0f26217aab44a8b7482e5379ce461530
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-03-25 17:13:55 -0500

    Step 5 Done

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 
 export default class FilterString extends Component {
-  render() {
+
+    constructor() {
+        
+        super();
+        
+        this.state = {
+            unFilteredArray: ["Mickey", "Mouse", "Company", "Not", "Fun", "anywhere"], 
+            userInput: '',
+            filteredArray: []}
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val });
+      }
+
+    filterStuff(userInput) {
+        let filtered = [];
+        
+        for (let i=0; i < this.state.unFilteredArray.length; i++) {
+            if (this.state.unFilteredArray[i].includes(userInput)) {
+                
+            } else {
+                filtered.push(this.state.unFilteredArray[i]);
+            }
+        }
+        this.setState({ filteredNames: filtered });
+    }
+
+
+
+
+    render() {
     return (
-      <p> FilterString Component </p>
+        <div className="puzzleBox filterStringPB">
+        <h4>Filter String</h4>
+        <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) } placeholder="Type in string to filter out"></input>
+        <button className="confirmationButton" onClick={ () => { this.filterStuff(this.state.userInput) }}>Get Results</button>
+        <span className="puzzleText"> Array to Filter: {this.state.unFilteredArray}</span>
+        <span className="resultsBox filterStringRB">Filtered: {this.state.filteredNames}</span>
+    </div>
     )
   }
 }
